76857d979cb2390a4a26666e27bd46d4366880b7
Muhammad Taufiqulsa`di <mtaufiqulsadi@gmail.com>
2023-09-23 14:07:02 +0700

    feat(hero-tier-statistics): add scss

--- src/pages/TierHeroesStatistics/TierHeroesStatistics.tsx
+++ src/pages/TierHeroesStatistics/TierHeroesStatistics.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import heroAPI from '../../api/hero';
 import {Data as Hero} from '../../api/hero/types';
 import { RANK_TYPE } from '../../api/hero/constants';
-
+import styles from './TierHeroesStatistics.module.scss';
 const TierHeroesStatisctics = ()=>{
 
   const [heroes, setHeroes] = useState<Hero[]>()
@@ -18,16 +18,26 @@ const TierHeroesStatisctics = ()=>{
   }, [])
 
   return (
-    <table>
-      {heroes?.map(({ban, avatar, name, use, win})=>(
+    <table className={styles['table-hero']}>  
+      <thead>
         <tr>
-          <td><img src={avatar} alt="" /></td>
-          <td>{name}</td>
-          <td>{ban}</td>
-          <td>{use}</td>
-          <td>{win}</td>
+          <th colSpan={2}>Hero</th>
+          <th>Win Rate</th>
+          <th>Use Rate</th>
+          <th>Ban Rate</th>
         </tr>
-      ))}
+      </thead>
+      <tbody>
+        {heroes?.map(({ban, avatar, name, use, win})=>(
+          <tr className={styles['item-hero-background']}>
+            <td><img src={avatar} alt="" className={styles['avatar-img']} /></td>
+            <td>{name}</td>
+            <td>{win}</td>
+            <td>{use}</td>
+            <td>{ban}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
