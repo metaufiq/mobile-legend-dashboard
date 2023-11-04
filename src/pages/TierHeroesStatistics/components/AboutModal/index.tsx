2ac75e9bb3bf0832b42399714e0b37239f0977ff
Muhammad Taufiqulsa`di <mtaufiqulsadi@gmail.com>
2023-11-04 09:43:33 +0700

    feat(hero-tier-statistics): fix about modal UI

--- src/pages/TierHeroesStatistics/components/AboutModal/index.tsx
+++ src/pages/TierHeroesStatistics/components/AboutModal/index.tsx
@@ -1,15 +1,27 @@
 import ReactModal from "react-modal"
 
 import { Props } from "./types"
+import { colors } from "../../../../styles/colors"
 
 const AboutModal = ({isOpen, overlayClassName}: Props)=>{
   return(
     <ReactModal
       isOpen={isOpen}
       overlayClassName={overlayClassName}
+      style={{
+        content:{
+          backgroundColor: colors.background,
+          color: "white",
+          height: 'fit-content',
+          width: 'fit-content',
+          top: 0,
+          bottom: 0,
+          margin: 'auto'
+        }
+      }}
     >
       <p>the data latest updated on: </p>
-      <p>Notes</p>
+      <p>Notes:</p>
       <ol>
         <li>All data in this leaderboard was collected from only Rank mode.</li>
         <li>Only the data of Epic and above has been counted.</li>
